3a3dcdf68a3ae976722f3b1be108360fb8d2c7b7
Deepak Singh <71811739+deepaksingh128@users.noreply.github.com>
2024-07-27 08:22:57 +0530

    Revised Aggressive Cows

---
+++ sortingANDsearching/medium/Aggressive_Cows.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static int solve(int n, int k, int[] stalls) {
+        Arrays.sort(stalls);
+        
+        int lo = 0;
+        int hi = stalls[n-1] - stalls[0];
+        int ans = 0;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (noOfCowsFind(stalls, mid) >= k) {
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid -1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    public static int noOfCowsFind (int[] stalls, int diff) {
+        int noOfCows = 1;
+        int currPos = 0;
+        
+        for (int i=1; i<stalls.length; i++) {
+            int currDiff = stalls[i] - stalls[currPos];
+            if (currDiff >= diff) {
+                noOfCows ++;
+                currPos = i;
+            }
+        }
+        
+        return noOfCows;
+    } 
+}
