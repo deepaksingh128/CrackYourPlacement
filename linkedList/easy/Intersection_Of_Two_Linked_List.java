e87cbf54c87674dd0db819cf7f69025134f65c04
Deepak Singh <71811739+deepaksingh128@users.noreply.github.com>
2024-07-28 22:00:42 +0530

    Revised Intersection Of Two Linked List

---
+++ linkedList/easy/Intersection_Of_Two_Linked_List.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // Approach 2 :- 
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        while (tempA != tempB) {
+            tempA = tempA != null ? tempA.next : headB;
+            tempB = tempB != null ? tempB.next : headA; 
+        }
+
+        return tempA;
+    }
+}
