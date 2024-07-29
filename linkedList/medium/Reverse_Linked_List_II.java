01bc2d161d1bcb89c3fd860c09729c8a842a1e33
Deepak Singh <71811739+deepaksingh128@users.noreply.github.com>
2024-07-29 11:28:54 +0530

    Solved Reverse Linked List II

---
+++ linkedList/medium/Reverse_Linked_List_II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        for (int i=1; i<left; i++) {
+            prev = prev.next;
+        }
+        ListNode curr = prev.next;
+
+        for (int i=1; i<=right-left; i++) {
+            ListNode temp = prev.next;
+            prev.next = curr.next;
+            curr.next = curr.next.next;
+            prev.next.next = temp;
+        }
+
+        return dummy.next;
+    }
+}
