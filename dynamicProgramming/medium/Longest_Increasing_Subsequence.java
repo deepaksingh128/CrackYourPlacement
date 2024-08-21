34298c38c95efd45e4b588193bbe44691e51f96c
Deepak Singh <71811739+deepaksingh128@users.noreply.github.com>
2024-08-21 22:59:31 +0530

    Updated Longest_Increasing_Subsequence.java

--- dynamicProgramming/medium/Longest_Increasing_Subsequence.java
+++ dynamicProgramming/medium/Longest_Increasing_Subsequence.java
@@ -1,6 +1,32 @@
+// Method 2:- Bottom Up
+//  t.c. => O(n^2) , s.c.=> O(n)
 class Solution {
-    // Method 1 :- (Recursion + Memoization)
+    
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
 
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+    
+        int maxLIS = 1;
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    maxLIS = Math.max(maxLIS, dp[i]);
+                }
+            }
+        }
+
+        return maxLIS;
+    }
+}
+
+
+// Method 1 :- (Recursion + Memoization)
+class Solution {
+   
     private int solve (int[] nums, int i, int prev, int[][] dp) {
         if (i >= nums.length) {
             return 0;
@@ -34,3 +60,6 @@ class Solution {
         return solve (nums, 0, -1, dp);
     }
 }
+
+
+ 
