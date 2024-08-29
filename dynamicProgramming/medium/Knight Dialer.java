0a73c348be16fd9059c17a004ec5acafb920dbb6
Deepak Singh <71811739+deepaksingh128@users.noreply.github.com>
2024-08-29 10:44:37 +0530

    Update Knight Dialer.java

--- dynamicProgramming/medium/Knight Dialer.java
+++ dynamicProgramming/medium/Knight Dialer.java
@@ -46,3 +46,51 @@ class Solution {
         return result;
     }
 }
+
+
+// Approach 2 : Bottom Up (Tabulation)
+
+class Solution {
+
+    int MOD = (int)1e9+7;
+    List<List<Integer>> adj = new ArrayList<>();
+
+    public int knightDialer(int n) {
+        adj.add(Arrays.asList(4, 6));    
+        adj.add(Arrays.asList(6, 8)); 
+        adj.add(Arrays.asList(7, 9));   
+        adj.add(Arrays.asList(4, 8));   
+        adj.add(Arrays.asList(3, 9, 0)); 
+        adj.add(new ArrayList<>());      
+        adj.add(Arrays.asList(1, 7, 0)); 
+        adj.add(Arrays.asList(2, 6));    
+        adj.add(Arrays.asList(1, 3));    
+        adj.add(Arrays.asList(2, 4));
+
+        int result = 0;
+        int[][] dp = new int[n][10];
+        // Base case :-
+        for (int cell=0; cell <=9; cell++) {
+            dp[0][cell] = 1;
+        }
+
+        for (int i=1; i<=n-1; i ++) {
+
+            for (int cell=0; cell<=9; cell++) {
+
+                int ans = 0;
+                for (int nextCell: adj.get(cell)) {
+                    ans = (ans + dp[i-1][nextCell]) % MOD;
+                }
+                dp[i][cell] = ans;
+            }
+        }
+
+        for (int cell=0; cell<=9; cell++) {
+            result = (result + dp[n-1][cell]) % MOD;
+        }
+
+
+        return result;
+    }
+}
